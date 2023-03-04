8c3cc98ade9d19e2e0c3c467f57973c0f18d61e7
wolfrit <wolfrit.company@gmail.com>
2023-03-04 19:46:48 +0100

    project part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+//components
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PageNoFoundComponent } from './pages/pageNoFound/page-no-found.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    PageNoFoundComponent
   ],
   imports: [
     BrowserModule
